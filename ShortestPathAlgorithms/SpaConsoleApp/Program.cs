263d7dff5546475d1047546a83f7e2ed3b15f00f
Jonas Umaras <jonas.umaras@gmail.com>
2022-04-30 19:08:47 +0300

    Update simple SPA console app.

--- ShortestPathAlgorithms/SpaConsoleApp/Program.cs
+++ ShortestPathAlgorithms/SpaConsoleApp/Program.cs
@@ -8,12 +8,14 @@ using ShortestPathAlgorithms.CostCalculators;
 using ShortestPathAlgorithms.Interfaces;
 using ShortestPathAlgorithms.Models;
 
-var (graph, nodeToCoordinatesMap) = DirectedGraphFactory.CreateRectangularGrid(100, 100);
+const int gridSize = 500;
+
+var (graph, nodeToCoordinatesMap) = DirectedGraphFactory.CreateRectangularGrid(gridSize, gridSize);
 
 var costCalc = new TerrainCostCalculator(nodeToCoordinatesMap);
 
 var fromNode = graph.Nodes.First(n => n.Id == "0");
-var toNode = graph.Nodes.First(n => n.Id == "10200");
+var toNode = graph.Nodes.First(n => n.Id == ((gridSize + 1) * (gridSize + 1) - 1).ToString());
 
 var startState = new State
 {
