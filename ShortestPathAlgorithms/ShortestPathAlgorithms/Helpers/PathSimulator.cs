a9d010c3afcfeea2784ed2b11051938078623f23
Jonas Umaras <jonas.umaras@gmail.com>
2022-05-08 09:14:24 +0300

    Unit test for graph with non-FIFO time-dependent links.

---
+++ ShortestPathAlgorithms/ShortestPathAlgorithms/Helpers/PathSimulator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Graphs.Interfaces;
+using ShortestPathAlgorithms.CostCalculators.Interfaces;
+using ShortestPathAlgorithms.Interfaces;
+
+namespace ShortestPathAlgorithms.Helpers;
+
+public class PathSimulator
+{
+    public static IReadOnlyList<double> Simulate(IReadOnlyList<INode> path, IGraphDirected graph, ICostCalculator<double, IState> costCalculator, IState startState)
+    {
+        var stepCosts = new List<double>(path.Count - 1);
+
+        for (var j = 1; j < path.Count; j++)
+        {
+            var edge = graph.Edges.First(e => e.From.Id == path[j - 1].Id && e.To.Id == path[j].Id);
+            var (stepCost, newState) = costCalculator.Calculate(edge, startState);
+            startState = newState;
+            stepCosts.Add(stepCost);
+        }
+
+        return stepCosts;
+    }
+}
--- ShortestPathAlgorithms/ShortestPathAlgorithmsTests/DijkstraTests/DynamicDijkstraTests.cs
+++ ShortestPathAlgorithms/ShortestPathAlgorithmsTests/DijkstraTests/DynamicDijkstraTests.cs
@@ -1,12 +1,15 @@
-﻿using System.Linq;
-using Graphs.Factories;
+﻿using Graphs.Factories;
 using Graphs.Interfaces;
+using Graphs.Models;
 using Moq;
 using ShortestPathAlgorithms.Algorithms;
 using ShortestPathAlgorithms.CostCalculators.Interfaces;
 using ShortestPathAlgorithms.Interfaces;
 using ShortestPathAlgorithms.Models;
 using ShortestPathAlgorithmsTests.Fixtures;
+using System;
+using System.Linq;
+using ShortestPathAlgorithms.Helpers;
 using Xunit;
 
 namespace ShortestPathAlgorithmsTests.DijkstraTests;
@@ -90,6 +93,76 @@ public class DynamicDijkstraTests : IClassFixture<SimpleDynamicGraphFixture>
         
         // Assert
         Assert.Null(path);
+    }
+
+    [Fact]
+    // Non-FIFO links mean that sub-paths are not optimal themselves
+    public void Find_NonFifoLinks_ShouldFindSolution()
+    {
+        // Arrange
+        var nodes = new[]
+        {
+            new Node("0"),
+            new Node("1"),
+            new Node("2"),
+            new Node("3")
+        };
+        var edges = new[]
+        {
+            new EdgeDirected(nodes[0], nodes[1]),
+            new EdgeDirected(nodes[0], nodes[2]),
+            new EdgeDirected(nodes[1], nodes[2]),
+            new EdgeDirected(nodes[2], nodes[3]),
+        };
+        var graph = new GraphDirected(edges);
+
+        var costCalculatorMock = new Mock<ICostCalculator<double, IState>>();
+        costCalculatorMock
+            .Setup(x => x.Calculate(It.IsAny<IEdgeDirected>(), It.IsAny<IState>()))
+            .Returns<IEdgeDirected, IState>((edge, state) =>
+            {
+                var newState = new State { Time = state.Time.AddHours(1) };
+
+                switch (edge.From.Id)
+                {
+                    case "0" when edge.To.Id == "1":
+                    case "0" when edge.To.Id == "2":
+                    case "1" when edge.To.Id == "2":
+                    {
+                        return (1, newState);
+                    }
+
+                    case "2" when edge.To.Id == "3":
+                    {
+                        var cost = state.Time.Hour == 1 ? 3 : 1;
+                        return (cost, newState);
+                    }
+
+                    default:
+                    {
+                        throw new Exception("Unexpected edge in the test case.");
+                    }
+                }
+            });
+
+        var startNode = graph.Nodes.First(n => n.Id == "0");
+        var endNode = graph.Nodes.First(n => n.Id == "3");
+        var startState = new State { Time = DateTime.UtcNow.Date };
+
+        // Act
+        var path = DijkstraDynamic<IState>.Find(graph, startNode, endNode, costCalculatorMock.Object, startState);
+
+        // Assert
+        Assert.NotNull(path);
+
+        var calculatedPathCosts = PathSimulator.Simulate(path!.Nodes, graph, costCalculatorMock.Object, startState);
+        var expectedPathCosts = PathSimulator.Simulate(nodes, graph, costCalculatorMock.Object, startState);
+        Assert.Equal(calculatedPathCosts.Sum(), path.Cost);
+        Assert.Equal(expectedPathCosts.Sum(), path.Cost);
+
+        Assert.Equal(4, path.Nodes.Count);
+        Assert.Equal(3, path.Cost);
+        Assert.Equal(nodes, path.Nodes);
     }
 
     private static Mock<ICostCalculator<double, IState>> UnitCostCalculatorMock()
